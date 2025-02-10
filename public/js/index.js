1f9ce52ad8209322dd9d09322f52faa13f9d4a93
carlosega19 <cega2005@gmail.com>
2025-02-10 11:20:45 -0400

    almost finish real time products

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,38 @@
 const socket = io();
+
+
+
+
+/* REAL TIME FORM */
+const prodForm = document.getElementById("prod-form");
+prodForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const titleInput = document.getElementById("titleInput");
+    const descInput = document.getElementById("descInput");
+    const priceInput = document.getElementById("priceInput");
+    const stockInput = document.getElementById("stockInput");
+    const categoryInput = document.getElementById("categoryInput");
+    const statusInput = document.getElementById("statusInput");
+    socket.emit("add product", {
+        title: titleInput.value,
+        description: descInput.value,
+        price: priceInput.value,
+        stock: stockInput.value,
+        status: statusInput.checked,
+        category: categoryInput.value
+    });
+    titleInput.value = "";
+    descInput.value = "";
+    priceInput.value = 0;
+    stockInput.value = 0;
+    categoryInput.value = "";
+    statusInput.checked = false;
+});
+
+function updateView() {
+
+}
+
+socket.on("product added", () => {
+    
+});
--- src/sockets/socket.js
+++ src/sockets/socket.js
@@ -1,13 +1,20 @@
 import { Server } from "socket.io";
+import { ProductManager } from "../controllers/ProductManager.js";
 
 const setUpSocketServer = (httpServer) => {
     const io = new Server(httpServer);
 
     io.on('connection', (socket) => {
         console.log("Cliente conectado");
+
+        socket.on("add product", (prod) => {
+            const productManager = new ProductManager("./src/DB/products.json");
+            productManager.addProduct(prod);
+            socket.emit("product added");
+        });
+
+
     });
-
-
     console.log("--SOCKET CREADO--");
 }
 
